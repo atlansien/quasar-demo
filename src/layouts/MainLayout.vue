9a63b2b518d12ba54b82e76da965688a6b8c3577
Toki <46712701+atlansien@users.noreply.github.com>
2020-06-19 08:46:35 +0900

    :sparkles: メニュー作った

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,34 +11,56 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="test-subtitle1">{{ todaysDate }}</div>
+      </div>
+      <q-img
+        src="statics/mount.jpg"
+        class="header-image absolute-top"
+      />
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+      <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 185px); margin-top: 185px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="statics/mount.jpg" style="height: 185px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://avatars2.githubusercontent.com/u/46712701?s=460&u=673a2c5dbbc0ed399b74bbb1076b7111da01d1a8&v=4">
+            </q-avatar>
+            <div class="text-weight-bold">jin takata</div>
+            <div>@proghallelujah</div>
+          </div>
+        </q-img>
+      </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -47,6 +69,7 @@
 </template>
 
 <script>
+import { date } from 'quasar'
 import EssentialLink from 'components/EssentialLink'
 
 export default {
@@ -104,6 +127,22 @@ export default {
         }
       ]
     }
+  },
+  computed: {
+    todaysDate() {
+      let timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'YYYY年MM月DD日')
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .header-image {
+    height: 100%;
+    z-index: -1;
+    opacity: 0.2;
+    filter: grayscale(100%);
+  }
+  
+</style>
